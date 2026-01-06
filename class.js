c49509064fba32a1eb41a7f7abcc49c25f01cbeb
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-01-06 16:42:48 +0530

    rectangle class

--- class.js
+++ class.js
@@ -7,3 +7,19 @@ map.set('name','dhruv')
 map.set('age','21')
 
 console.log(map.get('name'))
+
+
+//Creating a class
+class Rectangle {
+    constructor(width,height){
+        this.width = width 
+        this.height = height
+    }
+    area(){
+        const area = this.width * this.height
+        return area
+    }
+}
+const rect = new Rectangle(2,5)
+const area = rect.area()
+console.log(area)
