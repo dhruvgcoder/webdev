3ac680932ef31cf789b08e93b0759d0df88f273a
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-01-07 22:37:28 +0530

    todos using dom manipulation

--- todo.js
+++ todo.js
@@ -5,3 +5,15 @@ function addTodo() {
   console.log(value);
 }
 
+const h4Ele = document.querySelector("h4");
+console.log(h4Ele);
+
+const h4EleAll = document.querySelectorAll("h4");
+console.log(h4EleAll);
+console.log(h4EleAll[1]);
+
+const btn = document.getElementById("btn");
+console.log(btn);
+
+const todo = document.getElementsByClassName("todo");
+console.log(todo);
