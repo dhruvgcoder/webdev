5fdda48d880efb4015209da84f7463b366bc9d8b
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-01-09 22:27:41 +0530

    commit edited

--- promiseRevise.js
+++
@@ -1,7 +0,0 @@
-function setTimeoutPromisified(time){
-    return new Promise(resolve => setTimeout(resolve,time))
-}
-function callbackFn(){
-    console.log("you waited 10s")
-}
-setTimeoutPromisified(10000).then(callbackFn)
