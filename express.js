a3e3b7972029f0e373c2456ad1f0b53890f3caa2
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-01-10 22:41:01 +0530

    /movie route

--- express.js
+++ express.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
 
+function isOldEnough(req,res,next){
+    let age = req.query.age;
+    if(age>14){
+        next();
+    }else{
+        res.json({
+            msg:"You are not old enough to watch this movies"
+        })
+    }
+}
+
+app.get('/moviename', isOldEnough ,function(req,res){
+        res.json({
+            id:"nf_1344",
+            movie_name : "breaking bad"
+        })
+})
+
+
 app.listen(3000,function(){
     console.log("Server Started Successfuly on Port 3000")
 });
