9b9c93f3910c02644160d94f823e9219327e5757
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-01-15 06:28:08 +0530

    Another component in diff file

---
+++ react/src/Post.jsx
@@ -0,0 +1,15 @@
+export default function Post(){
+    return (
+        <PostComponent />
+    )
+} 
+
+function PostComponent(){
+    return(
+        <div>
+            <b>Welcome to bounties page</b>
+            <h3> 100$ bounty eligibility rules : </h3>
+            <p>Minimum 1 PR merged in 100x repo</p>
+        </div>
+    )
+}
