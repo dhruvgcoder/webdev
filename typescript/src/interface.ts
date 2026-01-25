83f72e8886dc64e9088c8c0e13b3f76da0d7ad35
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-01-25 12:01:16 +0530

    optional object in interface

--- typescript/src/interface.ts
+++ typescript/src/interface.ts
@@ -13,4 +13,29 @@ UserAge({
     lastname : "gupta",
     age : 21})
 
-    
+interface Report {
+    report_id : number,
+    report_summary : string
+}
+
+interface BugReport {
+    id : number ;
+    description : string;
+    report ?: Report ; // We defined report as type
+    solved : boolean ;
+    time ?: Date;  // ?: is used for optional data
+
+}
+
+function BugCheck(data : BugReport){
+    return data.solved ;
+}
+
+const bugInfo = {
+    id : 27362,
+    description : "issue in payment page",
+    solved : true
+}
+
+let bugSolved = BugCheck(bugInfo)
+console.log(bugSolved)
