3874fa6daa3a68aaab467d616ff9e679174526d4
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-01-25 21:04:26 +0530

    Enums in ts

---
+++ typescript/src/enums.ts
@@ -0,0 +1,51 @@
+
+enum UserResponse { 
+    No = 0 , // distinct value 
+    Yes = 1
+}
+
+function response(message: UserResponse){
+    return message;
+}
+
+let userResponse = response(UserResponse.Yes)
+console.log(userResponse)
+
+//String enums 
+
+enum Direction {
+    Up = "UP",
+    Down = "DOWN" ,
+    Left = "LEFT",
+    Right = "RIGHT"
+}
+
+function doSomething(keypress: Direction){
+    //do something
+}
+
+doSomething(Direction.Up)
+
+enum BooleanLikeHeterogenousEnum{
+    No = 0 ,
+    Yes = "YES"
+}
+
+// Common usecase 
+
+enum ResponseStatus {
+    Success = 200 ,
+    NotFound = 404 ,
+    Error = 500
+}
+
+app.get('/',(req,res)=>{
+    if(!req.query.userId){
+        res.status(ResponseStatus.Error).json({})
+    }
+    else {
+        res.status(ResponseStatus.Success).json({
+            msg : "Welcome xyz"
+        })
+    }
+})
