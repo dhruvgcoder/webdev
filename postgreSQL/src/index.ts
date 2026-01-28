7b5f2c97f9116146ff774c09ffbdd2a5e33fd63d
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-01-28 16:00:34 +0530

    Transactions in sql

--- postgreSQL/src/index.ts
+++ postgreSQL/src/index.ts
@@ -12,9 +12,23 @@ app.post("/signup",async (req,res)=>{
     const username = req.body.username 
     const email = req.body.email
     const password = req.body.password
+
+    const street = req.body.street
+    const city = req.body.city
+    const country = req.body.country
+    const pincode = req.body.pincode
+
 try {
-    let insertQuery = `INSERT INTO users (username , email , password ) VALUES ($1 , $2 , $3)`
+    const insertQuery = `INSERT INTO users (username , email , password ) VALUES ($1 , $2 , $3) RETURNING id`
+    const addressInsertQuery = `INSERT INTO addresses (street , city , country , pincode , user_id) VALUES ($1 , $2 , $3 , $4 , $5)`
+    
+    // TRANSACTIONS IN SQL 
+    await pgClient.query('BEGIN')
     const response = await pgClient.query(insertQuery , [username , email , password])
+    const userId = response.rows[0].id
+    const addressInsertResponse = await pgClient.query(addressInsertQuery , [street , city , country , pincode, userId])
+    await pgClient.query('COMMIT')
+
     res.status(200).json({
         msg : "You have signed up"
     })
@@ -24,6 +38,20 @@ try {
     })
 }
 })
+
+app.get('/metadata', async(req,res)=>{
+    const id = req.query.id 
+
+    const query1 = `SELECT username , email , password FROM users WHERE id=$1`
+    const response1 = await pgClient.query(query1 , [id])
+
+    const query2 = `SELECT * FROM addresses WHERE user_id=$1`
+    const response2 = await pgClient.query(query2 , [id])
+    res.json({
+        user : response1.rows[0] ,
+        address : response2.rows[0]
+    })
+}) 
 app.listen(3000,()=>{
     console.log("Server is running on PORT 3000")
 })
