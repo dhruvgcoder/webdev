88ef2ae834f4a2efd7cbf4e9a346ff694f2bac4b
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-01-28 10:15:13 +0530

    SQL injection check

--- postgreSQL/src/index.ts
+++ postgreSQL/src/index.ts
@@ -1,19 +1,29 @@
 import { Client } from "pg";
-import dotenv from "dotenv";
+import "dotenv/config";
+import express from "express";
 
-dotenv.config()
+const app = express();
+app.use(express.json());
 
-const pgClient = new Client({
-    user : process.env.PG_USER,
-    password : process.env.PG_PASSWORD,
-    port : 5432,
-    host : process.env.PG_HOST ,
-    database : "neondb"
-})
+const pgClient = new Client(process.env.PG_URL)
+pgClient.connect();
 
-async function main(){
-    await pgClient.connect();
-
-    const response = pgClient.query("SELECT * FROM users")
-    console.log(response)
+app.post("/signup",async (req,res)=>{
+    const username = req.body.username 
+    const email = req.body.email
+    const password = req.body.password
+try {
+    let insertQuery = `INSERT INTO users (username , email , password ) VALUES ($1 , $2 , $3)`
+    const response = await pgClient.query(insertQuery , [username , email , password])
+    res.status(200).json({
+        msg : "You have signed up"
+    })
+}catch(e){
+    res.json({
+        msg : "Error during signin"
+    })
 }
+})
+app.listen(3000,()=>{
+    console.log("Server is running on PORT 3000")
+})
