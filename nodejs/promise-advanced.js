a5ed4ff8191d7b561926c301775a3d42c23afe6b
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-02-13 23:15:50 +0530

    Promise any

--- nodejs/promise-advanced.js
+++ nodejs/promise-advanced.js
@@ -21,3 +21,33 @@ Promise.race([slowOperation(), fastOperation(), superFastOperation()]).then((res
   console.log("Race result:", result); 
 });
 
+// PROMISE.ANY
+
+function operation1() {
+  return new Promise((resolve, reject) =>
+    setTimeout(() => reject("Operation 1 failed"), 1000)
+  );
+}
+
+function operation2() {
+  return new Promise((resolve) =>
+    setTimeout(() => resolve("Operation 2 succeeded"), 2000)
+  );
+}
+
+function operation3() {
+  return new Promise((resolve, reject) =>
+    setTimeout(() => reject("Operation 3 failed"), 500)
+  );
+}
+
+// Promise.any returns first fulfilled promise, ignoring rejections
+Promise.any([operation1(), operation2(), operation3()])
+  .then((result) => {
+    console.log("First successful operation:", result); // "Operation 2 succeeded"
+  })
+  .catch((error) => {
+    console.log("All operations failed:", error);
+  });
+
+
